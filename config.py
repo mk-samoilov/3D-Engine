1a78841f677719ff791cf98acd007336c74bb6e2
mk-samoilov <me@mk-samoilov.ru>
2025-03-13 19:59:18 +0300

    777Commit777

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 WINDOW_WIDTH = 1920
 WINDOW_HEIGHT = 1080
 WINDOW_TITLE = "MK-3D-ENGINE-WINDOW"
-WINDOW_ICON = "engine3d/engine-icon.png"
+WINDOW_ICON = "./engine3d/engine-icon.png"
 
 DRAW_DISTANCE = 1000
 MSAA_X = 8
